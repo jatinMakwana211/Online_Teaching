b3218d03e04e19249f9e2f7635b46b13585042a2
jatinMakwana211 <jatinmakwana5000@gmail.com>
2023-12-31 10:32:33 +0530

    Even_number,Breaktime,while,forloop

--- Online_Lecture_webpage/index.js
+++ Online_Lecture_webpage/index.js
@@ -1,3 +1,57 @@
-var average=(39+18+10+51+27+69+31)/7
-console.log(average+" minutes per day")
-console.log(average>=30)
+/*var BreakTime=[]
+var Round=1
+
+function addBreakTime(){
+    if (Round%4==0){
+        BreakTime.push(15)
+    }
+    else{
+        BreakTime.push(5)
+    }
+    Round++;
+    console.log(BreakTime)
+}
+
+var counter=1
+while(counter<=16){
+    addBreakTime()
+    counter++;
+}
+console.log(counter)
+
+for (var i=1; i<=10; i++){
+    console.log(i)
+    
+}*/
+
+var Mylist=[13,23,12,45,22,48,66,100];
+
+function printeven(list){
+    for(var i=0;i<list.length;i++){
+        if(list[i]%2==0){
+            console.log(list[i])
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function printeven(list){
+    for(var i=0;i<list.length;i++){
+        if(list[i]%2 == 0){
+            console.log(list[i]);
+        }
+    }
+}
+printeven(Mylist)
